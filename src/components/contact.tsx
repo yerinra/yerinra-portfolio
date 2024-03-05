f95832ba6e03db74c3154f78d565aa2ac5cceb5e
yerinra <yerinaleph@gmail.com>
2024-03-05 20:56:55 +0900

    ✨ Feat: submit btn 컴포넌트

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -2,10 +2,11 @@
 
 import React from "react";
 import SectionHeading from "./section-heading";
-import { PaperPlaneIcon } from "@radix-ui/react-icons";
 import { motion } from "framer-motion";
 import { useSectionInView } from "@/lib/hooks";
-import { sendEmail } from "../../actions/sendEmail";
+import { sendEmail } from "../actions/sendEmail";
+import { useFormStatus } from "react-dom";
+import SubmitBtn from "./submit-btn";
 
 export default function Contact() {
   const { ref } = useSectionInView("Contact");
@@ -49,13 +50,7 @@ export default function Contact() {
           required
           maxLength={1000}
         />
-        <button
-          type="submit"
-          className="group flex items-center justify-center gap-2 h-[3rem] w-[8rem] bg-gray-900 text-white rounded-full outline-none transition-all focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105"
-        >
-          Submit
-          <PaperPlaneIcon className="text-xs opacity-70 transition-all group-hover:translate-x-1 group-hover:-translate-y-1" />
-        </button>
+        <SubmitBtn />
       </form>
     </motion.div>
   );
